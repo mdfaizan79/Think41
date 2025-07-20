2dea8d84522558ef60868ed1be663a21528e0011
Md Faizan <mdfaizan@Mds-MacBook-Air.local>
2025-07-20 17:09:48 +0530

    Change in Index.js

--- index.js
+++ index.js
@@ -1,33 +1,24 @@
 import express from 'express';
 import mongoose from 'mongoose';
-import dotenv from 'dotenv';
-import taskRoutes from './Routes/TaskRoutes.js'
+import baggageRoutes from './routes/baggageRoutes.js';
 
 const app = express();
-const port = process.env.PORT || 8000;
-
-
-
-dotenv.config({});
-
-
 app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URI,{
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-})
- .then(() => 
-    console.log('MongoDB connected Successfully ')
-)
- .catch(()=> 
-    console.log('Failed to Connect'))
-
-app.use('/tasks', taskRoutes);
-
-app.listen(port,()=>{
-    console.log(`Server running on port ${port}`)
-}
-);
+mongoose.connect('mongodb://localhost:27017/baggageDB', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).then(() => {
+  console.log("MongoDB connected");
+}).catch(err => {
+  console.error("MongoDB connection failed:", err);
+});
 
 
+app.use('/baggage', baggageRoutes);
+
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
